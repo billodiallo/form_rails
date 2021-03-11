e78b33c5cd381b01b9feb68f13785396b08b4ac8
Billo Diallo <billodiallonet@gmail.com>
2021-03-11 19:31:33 +0300

    Fix linters Errors

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,4 +1,2 @@
-# rubocop: disable Layout/EndOfLine
 module UsersHelper
 end
-# rubocop: enable Layout/EndOfLine
--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,4 @@
-# rubocop: disable Layout/EndOfLine
 class ApplicationMailer < ActionMailer::Base
   default from: 'from@example.com'
   layout 'mailer'
 end
-# rubocop: enable Layout/EndOfLine
